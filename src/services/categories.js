c2967556da82f6adf4567b01e4a2bae52fa8480e
Marco Romero <marcod.romero@gmail.com>
2023-09-16 14:41:09 -0300

    fix export categories

--- src/services/categories.js
+++ src/services/categories.js
@@ -1,13 +1,9 @@
 const baseUrl = `${process.env.REACT_APP_SERVERIP}/categories`;
 
-const getCategories = async () => {
+export const getCategories = async () => {
   const apiUrl = baseUrl;
   const response = await fetch(apiUrl, {
     method: "GET",
   });
   return response;
-};
-
-module.exports = {
-  getCategories,
 };
